58af69417cec8b43e1948a952e05727bc6c2cc35
ezroot <utohaha@gmail.com>
2023-09-08 01:08:46 -0400

    cool attack animation

--- Assets/_Assets/Scripts/Commands/AttackCommand.cs
+++ Assets/_Assets/Scripts/Commands/AttackCommand.cs
@@ -22,19 +22,33 @@ namespace TickBased.Scripts.Commands
 
         public IEnumerator Execute()
         {
-            // Animation or effect for attacking
-            // e.g. Rotate the attacker towards the target
-            Vector2 direction = _targetCreatureEntity.CreatureTransform.position - _currentCreatureEntity.CreatureTransform.position;
+            // Save original position
+            Vector2 originalPosition = _currentCreatureEntity.CreatureTransform.position;
+            Vector2 targetPosition = _targetCreatureEntity.CreatureTransform.position;
+            // Rotate the attacker towards the target
+            Vector2 direction = targetPosition - originalPosition;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
             _currentCreatureEntity.SetDirection(Quaternion.AngleAxis(angle, Vector3.forward));
+            yield return new WaitForSeconds(0.25f); // Delay for some effect or animation
 
-            // yield return new WaitForSeconds(0.2f); // Delay for some effect or animation
+            // Move to target position instantly
+            _currentCreatureEntity.CreatureTransform.position = _targetCreatureEntity.CreatureTransform.position;
+
 
             // Apply damage logic
-            // e.g. targetEntity.TakeDamage(damage);
             _targetCreatureEntity.EntityData.TakeDamage(_attackDamage);
 
-            yield return null;
+            // Lerp back to original position
+            float lerpDuration = 0.25f;
+            float lerpElapsed = 0;
+            while (lerpElapsed < lerpDuration)
+            {
+                lerpElapsed += Time.deltaTime;
+                _currentCreatureEntity.CreatureTransform.position = Vector2.Lerp(_targetCreatureEntity.CreatureTransform.position, originalPosition, lerpElapsed / lerpDuration);
+                yield return null;
+            }
+
+            _currentCreatureEntity.CreatureTransform.position = originalPosition;
         }
     }
 }
